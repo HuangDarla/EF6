7fcbd757fd92e6f5592de7bcd6775eb85e5a8dba
Darla Huang <wonder0728@gmail.com>
2017-11-26 10:55:35 +0800

    開新專案

--- EF6/Program.cs
+++ EF6/Program.cs
@@ -10,6 +10,37 @@ namespace EF6
     {
         static void Main(string[] args)
         {
+            using (var db = new ContosoUniversityEntities())
+            {
+                db.Database.Log = (sql) => { Console.WriteLine(sql); };
+
+                //GetCourse_Git(db);
+
+                var data = (from p in db.Department.Include("Course") select p); //
+                foreach (var dept in data)
+                {
+                    Console.WriteLine(dept.Name);
+                    foreach (var course in dept.Course)
+                    {
+                        Console.WriteLine("\t" + course.Title);
+                    }
+                }
+
+            }
+            Console.ReadLine();
+        }
+
+        private static void GetCourse_Git(ContosoUniversityEntities db)
+        {
+            //var data = db.Course.Where(p => p.Title.Contains("Git")).ToList();
+            var data = (from p in db.Course
+                        where p.Title.Contains("Git")
+                        select p).ToList();
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Title + "\t" + item.Department.Name);
+            }
         }
     }
 }
